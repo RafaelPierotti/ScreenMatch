ad26ca130fd29a9be3898a6dd4c07790af2a994f
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-10-18 22:18:13 -0300

    Modelos

--- src/br/com/screenmatch/modelos/Titulo.java
+++ src/br/com/screenmatch/modelos/Titulo.java
@@ -66,4 +66,9 @@ public class Titulo implements Comparable<Titulo>{
     public int compareTo(Titulo outroTitulo){
         return this.getNome().compareTo(outroTitulo.getNome());
     }
+
+    @Override
+    public String toString(){
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLançamento() + ")";
+    }
 }
