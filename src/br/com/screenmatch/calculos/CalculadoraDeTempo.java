296ccde23c944a93869f4633635a5ed0429ecd79
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-10-18 21:58:40 -0300

    Calculadora de Tempo

---
+++ src/br/com/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,18 @@
+package br.com.screenmatch.calculos;
+
+import br.com.screenmatch.modelos.Filme;
+import br.com.screenmatch.modelos.Serie;
+import br.com.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
