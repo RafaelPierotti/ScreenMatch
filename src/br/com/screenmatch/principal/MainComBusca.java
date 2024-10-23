c7d8c59b4170d2ff1bc469b0874acda666991bd1
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-10-23 17:54:17 -0300

    API Pesquisando

--- src/br/com/screenmatch/principal/MainComBusca.java
+++ src/br/com/screenmatch/principal/MainComBusca.java
@@ -1,14 +1,29 @@
 package br.com.screenmatch.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual filme você deseja pesquisar?");
+        var filme = scanner.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + filme + "&apikey=43a88045";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(URI.create(url))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
     }
 }
+
